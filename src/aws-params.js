1f8948653d76b9da3d8a122f09923da884dfc1ed
Carlos Torres <ctgovea@gmail.com>
2020-10-03 15:36:58 -0500

    Update aws-params.js

--- src/aws-params.js
+++ src/aws-params.js
@@ -6,13 +6,13 @@ const awsparams = {
     "aws_cloud_logic_custom": [
         {
             "name": "AmplifyWebApp",
-            "endpoint": "https://r8ikpkybxk.execute-api.us-east-1.amazonaws.com/dev",
+            "endpoint": "https://prg6w8g3y2.execute-api.us-east-1.amazonaws.com/dev",
             "region": "us-east-1"
         }
     ],
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_OsMLSvOmG",
-    "aws_user_pools_web_client_id": "7qr856n6qrnjdt6kirrp7vajtf",
+    "aws_user_pools_id": "us-east-1_nnOqn87te",
+    "aws_user_pools_web_client_id": "42n19u7o9me2f2s05rmrv43pdi",
     "oauth": {},
     "app_linking": {
         "COGNITO_AUTH_URL": 'Value for your Cognito Authorization URL',
